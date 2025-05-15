c7f972c7de48de0162483855a108e2f826496391
M0rona <gabrielmorona0229@gmail.com>
2025-05-15 09:29:42 -0300

    Removido node modules (joao mto burro), adicionado logica de cors local e movido a logica para pegar o token dos cookies

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
